1e6834ac94d944e319346ab541926b0df6096fc4
blerim <blerimballiu19@gmail.com>
2023-10-16 07:58:25 -0700

    3. alphABET

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char letter;
+
+for (letter = 'a'; letter <= 'z'; letter++)
+for (letter = 'A'; letter <= 'Z'; letter++)
+putchar(letter);
+  putchar('\n');
+
+
+return (0);
+
+}
