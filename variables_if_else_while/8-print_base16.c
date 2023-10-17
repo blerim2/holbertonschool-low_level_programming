d5dd1b6e0463bc292579f9d04fd734c460d94838
blerim <blerimballiu19@gmail.com>
2023-10-17 05:33:08 -0700

    8. Hexadecimal

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int a;
+char letter;
+
+for (a = 0; a < 10; a++)
+putchar(a + '0');
+for (letter = 'a'; letter < 'f'; letter++)
+putchar (letter);
+ putchar('\n');
+
+
+return (0);
+
+}
