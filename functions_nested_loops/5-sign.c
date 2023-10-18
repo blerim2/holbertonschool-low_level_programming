68b01c9562b7d2eef1b7234278b37169562831d6
blerim <blerimballiu19@gmail.com>
2023-10-18 05:43:20 -0700

    5. Sign12

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ ** _isalpha - checks for an alphabetic character; in the standard "C" locale
+ ** @c: the character to check
+ ** Return: 1 if c is a letter, lowercase or uppercase, 0 otherwise
+ **/
+
+int print_sign(int n)
+{
+		if (n > 0)
+		{
+			_putchar('+');		       
+		return (1);
+		}
+		else if (n == 0)
+		{
+		_putchar('0');
+			return (0);
+		}
+		else
+		{
+		_putchar('-');
+		return (-1);
+		}
+}
