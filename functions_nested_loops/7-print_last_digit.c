58d275433e2ccf0b8b93ff8e1447e11f554b0577
blerim <blerimballiu19@gmail.com>
2023-10-18 08:23:01 -0700

    6. There is no such thing as absolute value in this world

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ ** _abs - Computers the absolute
+ ** @c: The number computed
+ ** Return: Absolute value
+ **/
+
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+		else
+	return (n);
+}
