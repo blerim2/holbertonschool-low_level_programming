9ad33e09a54387a4fa019535feb0299067846b3c
blerim <blerimballiu19@gmail.com>
2023-10-19 05:55:13 -0700

    knkfwnekwn

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -2,10 +2,11 @@
 
 /**
  * add - adds two intefers and returns result
- * @a: int to be add b
- * @: int to be add a
+ * @a: int to be added to b
+ * @b: int to be added to a
  * Return: sum of a and b
  */
+
 int add(int a, int b)
 
 {
