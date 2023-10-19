ab61b9f286a3883834e9bd82f433f5ff84dffd2f
blerim <blerimballiu19@gmail.com>
2023-10-19 05:52:47 -0700

    knkfwnekwn

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two intefers and returns result
+ * @a: int to be add b
+ * @: int to be add a
+ * Return: sum of a and b
+ */
+int add(int a, int b)
+
+{
+	return (a + b);
+}
