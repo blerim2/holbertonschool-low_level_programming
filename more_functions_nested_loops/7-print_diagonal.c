4cc6cb6b3631a088a987c87e9acb4ce79310317d
blerim <blerimballiu19@gmail.com>
2023-10-20 03:52:47 -0700

    7. I feel like I am diagonally park

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -7,23 +7,19 @@
  **/
 void print_diagonal(int n)
 {
-	if (n <= 0)
-	{
-		_putchar ('\n');
-	}
-	else
-	{
-	 int i, j;
-		for (i = 0; i <= n; i++)
+	int i, j;
+		if (n > 0)
 		{
-		for (j = 0; j <= n; j++)
-		{
-		if (j == i)
-		_putchar('\\');
-			else if (j < i)
+			for (i = 0; i < n; i++)
+			{
+			for (j = 0; j < i; j++)
+			{
 			_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+			}
 		}
-		}
-		}
-	_putchar ('\n');
-}
+			else
+				_putchar('\n');
+}	
