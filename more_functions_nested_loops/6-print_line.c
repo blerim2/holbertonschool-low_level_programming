5529c3e538e18f6490d92c1fd73227a26e3537ec
blerim <blerimballiu19@gmail.com>
2023-10-20 03:15:28 -0700

    6. The shortest distance between

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- ** main - check the code
- **
+ ** print_line - check for a straight line
+ ** @n: h
  ** Return: Always 0.
  **/
 
 void print_line(int n)
 {
-	if ( n <= 0)
+	if (in <= 0)
 	{
 		_putchar ('\n');
 		return;
