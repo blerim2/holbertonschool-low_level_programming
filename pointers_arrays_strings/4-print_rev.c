2e536f7f7c51df313b911d25f04b599bafff1975
blerim <blerimballiu19@gmail.com>
2023-10-23 04:39:28 -0700

    4. I can only go one way

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -4,16 +4,17 @@
 
 void print_rev(char *s)
 {
-	int len = 0;
+	int len;
 	int i, last_index;
-	while(s[len] != '\0')
+		while(s[len] != '\0')
 	{
 		len++;
-	last_index = len -1;
 	}
+	last_index = len -1;
+	
 	for (i=last_index; i>=0; i--)
 	{
-		putchar(s[i]);
+		_putchar(s[i]);
 	}
-	putchar('\n');
+	_putchar('\n');
 }
