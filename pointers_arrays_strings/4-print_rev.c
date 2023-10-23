6b46245eb623b660d52a2a5d6e8a7905e0c705dd
blerim <blerimballiu19@gmail.com>
2023-10-23 04:47:12 -0700

    4. I can only go one way

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,18 +1,22 @@
 #include "main.h"
 
-
+/**
+ ** print_rev - prints a string, in reverse, followed by a new line.
+ ** @s: input string.
+ ** Return: no return.
+ **/
 
 void print_rev(char *s)
 {
 	int len;
-	int i, last_index;
+	int i,last_index;
 		while(s[len] != '\0')
 	{
 		len++;
 	}
 	last_index = len -1;
 	
-	for (i=last_index; i>=0; i--)
+	for (i = last_index; i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}
