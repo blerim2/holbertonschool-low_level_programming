8b32da558b97196c86b1915c415cce4d1ba2616d
blerim <blerimballiu19@gmail.com>
2023-10-23 03:06:47 -0700

    0. 98 Battery st.

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
-/*
- * reset_to_98-updates the value
- * @*n: reset to 
- * Return: Always 0.
+/**
+ * reset_to_98 - updatesthevalue
+ * @n: update the value
  */
 
 void reset_to_98(int *n)
