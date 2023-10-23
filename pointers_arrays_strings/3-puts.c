25dd05c44305b5ce9c7be56e67250cb620e5ea97
blerim <blerimballiu19@gmail.com>
2023-10-23 04:34:47 -0700

    4. I can only go one way

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -3,6 +3,14 @@
 /**
  ** reset_to_98 - updatesthevalue
  ** @n: update the value
- **/
+**/
+
 void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+		{
+	_putchar(*str);
+		}
+	_putchar('\n');
+}
 
