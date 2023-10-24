6eb7daba1b62e7eb4fa06cea9794f9eb83507509
blerim <blerimballiu19@gmail.com>
2023-10-24 01:31:18 -0700

    6. Half the lies they tell about me aren't true

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
-/*
- * _puts - prints a string, followed by a new line.
- * @str: input string.
- * Return: no return.
- */
+/**
+ ** puts2 - prints every other character of a string
+ ** @str: input string.
+ ** Return: no return.
+ **/
 
 void puts2(char *str)
 {
 	int len = 0;
 	int i;
 		while (str[len] != '\0')
-		{	
+		{
 			len++;
-		}	
-		for(i = 0; i < len; i += 2)
+		}
+		for (i = 0; i < len; i += 2)
 		{
 		_putchar(str[i]);
 		}
