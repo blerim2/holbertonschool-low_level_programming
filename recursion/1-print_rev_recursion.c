6d85f9f0126f56a9e806541e3e77f4ad05889cfa
blerim <blerimballiu19@gmail.com>
2023-10-30 06:08:36 -0700

    . Why is it so important to dream?

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ ** _print_rev_recursion - prints in reverse
+ **
+ ** @s: string pointer
+ **/
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
