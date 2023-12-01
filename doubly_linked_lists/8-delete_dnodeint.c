42244ebe4ec458d631759fd36c47ec45c15f38c9
blerim <blerimballiu19@gmail.com>
2023-12-01 02:04:02 -0800

    Delete at index

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - inserts a new node at a given position
+ * @h: pointer to the head of the list
+ * @idx: index of the list where the new node should be added
+ * @n: value to be added to the new node
+ *
+ * Return: the address of the new node, or NULL if it failed
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new_node, *temp = *h;
+	unsigned int i;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (!new_node)
+		return (NULL);
+	new_node->n = n;
+	if (!idx)
+	{
+		new_node->prev = NULL;
+		new_node->next = *h;
+		if (*h)
+			(*h)->prev = new_node;
+		*h = new_node;
+		return (new_node);
+	}
+	for (i = 0; temp && i < idx - 1; i++)
+		temp = temp->next;
+	if (!temp)
+		return (NULL);
+	new_node->next = temp->next;
+	new_node->prev = temp;
+	if (temp->next)
+		temp->next->prev = new_node;
+	temp->next = new_node;
+	return (new_node);
+}
+
