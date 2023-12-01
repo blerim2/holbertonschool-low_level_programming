9e8f37548afaba2c97087b82668edbd18b052d90
blerim <blerimballiu19@gmail.com>
2023-12-01 01:33:33 -0800

    Insert at index

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -10,30 +10,30 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    dlistint_t *new_node, *temp = *h;
-    unsigned int i;
+	dlistint_t *new_node, *temp = *h;
+	unsigned int i;
 
-    new_node = malloc(sizeof(dlistint_t));
-    if (!new_node)
-        return (NULL);
-    new_node->n = n;
-    if (!idx)
-    {
-        new_node->prev = NULL;
-        new_node->next = *h;
-        if (*h)
-            (*h)->prev = new_node;
-        *h = new_node;
-        return (new_node);
-    }
-    for (i = 0; temp && i < idx - 1; i++)
-        temp = temp->next;
-    if (!temp)
-        return (NULL);
-    new_node->next = temp->next;
-    new_node->prev = temp;
-    if (temp->next)
-        temp->next->prev = new_node;
-    temp->next = new_node;
-    return (new_node);
+	new_node = malloc(sizeof(dlistint_t));
+	if (!new_node)
+		return (NULL);
+	new_node->n = n;
+	if (!idx)
+	{
+	new_node->prev = NULL;
+	new_node->next = *h;
+	if (*h)
+	(*h)->prev = new_node;
+	*h = new_node;
+	return (new_node);
+	}
+	for (i = 0; temp && i < idx - 1; i++)
+	temp = temp->next;
+	if (!temp)
+	return (NULL);
+	new_node->next = temp->next;
+	new_node->prev = temp;
+	if (temp->next)
+	temp->next->prev = new_node;
+	temp->next = new_node;
+	return (new_node);
 }
