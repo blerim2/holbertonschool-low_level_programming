2d2acb464717be0edeacc7e1cb2601b3fa18c1a5
blerim <blerimballiu19@gmail.com>
2023-12-05 01:30:12 -0800

    Tread lightly, she is near

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,39 +1,39 @@
-#include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output
- * @filename: name of the file to read
- * @letters: number of letters it should read and print
+ * read_textfile - Reads a text file and prints it to POSIX stdout.
+ * @filename: A pointer to the name of the file.
+ * @letters: The number of letters the
+ *           function should read and print.
  *
- * Return: the actual number of letters it could read and print
+ * Return: If the function fails or filename is NULL - 0.
+ *         O/w - the actual number of bytes the function can read and print.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t n_read, n_written;
+	ssize_t o, r, w;
 	char *buffer;
-	FILE *fp;
 
-	if (!filename)
-		return (0);
-
-	fp = fopen(filename, "r");
-	if (!fp)
+	if (filename == NULL)
 		return (0);
 
 	buffer = malloc(sizeof(char) * letters);
-	if (!buffer)
+	if (buffer == NULL)
+		return (0);
+
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+
+	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
-		fclose(fp);
+		free(buffer);
 		return (0);
 	}
 
-	n_read = fread(buffer, sizeof(char), letters, fp);
-	n_written = fwrite(buffer, sizeof(char), n_read, stdout);
-
 	free(buffer);
-	fclose(fp);
+	close(o);
 
-	return (n_written);
+	return (w);
 }
-
