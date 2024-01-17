d2aae5e319b9ee5a4a84925b441e4b0a54560c0c
blerim2 <blerimballiu19@gmail.com>
2024-01-17 11:27:55 +0000

    Linear

---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,13 @@
+#ifndef SEARCH_ALGOS
+#define SEARCH_ALGOS
+
+#include <stdio.h>
+#include <stddef.h>
+#include <math.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+size_t min(size_t a, size_t b);
+
+#endif
